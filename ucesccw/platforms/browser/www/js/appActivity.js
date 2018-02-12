e7630a7359c0a96b0fa668a3debbab70fc46cf02
Cory Williams <ucesccw@ucl.ac.uk>
2018-02-12 12:10:25 -0500

    automatically load USGS earthquake data

--- ucesccw/platforms/browser/www/js/appActivity.js
+++ ucesccw/platforms/browser/www/js/appActivity.js
@@ -12,60 +12,77 @@
       maxZoom: 18,
 
       id: 'mapbox.streets'
+	  
+	 }).addTo(mymap);
 
-    }).addTo(mymap);
+	// create a variable that will hold the XMLHttpRequest() - this must be done outside a function so that all the functions can use the same variable 
 	
-	    // add a point
-
-    L.marker([51.5, -0.09]).addTo(mymap)
-
-      .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
-
-    // add a circle
-
-    L.circle([51.508, -0.11], 500, {
-
-      color: 'red',
-
-      fillColor: '#f03',
-
-      fillOpacity: 0.5
-
-    }).addTo(mymap).bindPopup("I am a circle.");
-
-    // add a polygon with 3 end points (i.e. a triangle)
-
-    var myPolygon = L.polygon([
-
-      [51.509, -0.08],
-
-      [51.503, -0.06],
-
-      [51.51, -0.047]
-
-    ], {
-
-      color: 'red',
-
-      fillColor: '#f03',
-
-      fillOpacity: 0.5
-
-    }).addTo(mymap).bindPopup("I am a polygon.");
+	var client;
 	
-		// create a custom popup
+	// and a variable that will hold the layer itself – we need to do this outside the function so that we can use it to remove the layer later on
 	
-	var popup = L.popup();
-	
-	// create an event detector to wait for the user's click event and then use the popup to show them where they clicked
+	var earthquakelayer;
 
-	// note that you don't need to do any complicated maths to convert screen coordinates to real world coordinates - the Leaflet API does this for you
-
-	function onMapClick(e) { 
-popup
-.setLatLng(e.latlng)
-.setContent("You clicked the map at " + e.latlng.toString())
-.openOn(mymap);
+	// create the code to get the Earthquakes data using an XMLHttpRequest
+	function getEarthquakes() {
+	client = new XMLHttpRequest();
+	client.open('GET','https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson');
+	client.onreadystatechange = earthquakeResponse; // note don't use earthquakeResponse() with brackets as that doesn't work
+	client.send();
 }
-	// now add the click event detector to the map
-mymap.on('click', onMapClick);
+
+	// create custom red marker
+	var testMarkerRed = L.AwesomeMarkers.icon({
+	icon: 'play',
+	markerColor: 'red'
+});
+
+	// create custom pink marker 
+	var testMarkerPink = L.AwesomeMarkers.icon({
+	icon: 'play',
+	markerColor: 'pink'
+});
+	// create the code to wait for the response from the data server, and process the response once it is received
+	
+	function earthquakeResponse() {
+	
+	// this function listens out for the server to say that the data is ready - i.e. has state 4
+	
+	if (client.readyState == 4) {
+	// once the data is ready, process the data
+	
+	var earthquakedata = client.responseText;
+	loadEarthquakelayer(earthquakedata);
+}
+}
+
+	// convert the received data - which is text - to JSON format and add it to the map
+	function loadEarthquakelayer(earthquakedata) {
+	
+	// convert the text to JSON
+	var earthquakejson = JSON.parse(earthquakedata);
+	
+	// load the geoJSON layer
+	var earthquakelayer = L.geoJson(earthquakejson,
+{
+	// use point to layer to create the points
+	pointToLayer: function (feature, latlng)
+{
+	// look at the GeoJSON file - specifically at the properties - to see the earthquake magnitude and use a different marker depending on this value
+	// also include a pop-up that shows the place value of the earthquakes
+	if (feature.properties.mag > 1.75) {
+	return L.marker(latlng, {icon:testMarkerRed}).bindPopup("<b>"+feature.properties.place +"</b>");
+}
+	else {
+	// magnitude is 1.75 or less
+	return L.marker(latlng, {icon:testMarkerPink}).bindPopup("<b>"+feature.properties.place +"</b>");;
+}
+},
+}).addTo(mymap);
+	
+	// change the map zoom so that all the data is shown
+	mymap.fitBounds(earthquakelayer.getBounds());
+}	
+	document.addEventListener('DOMContentLoaded', function() {
+	getEarthquakes();
+}, false);
